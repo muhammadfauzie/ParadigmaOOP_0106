5c57269d1cb3c2411fde685570d87779f24958aa
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-03 18:30:54 +0700

    membuat program class pekerja dengan menggunakan umur

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -10,3 +10,13 @@ class orang {
         cout << "orang dibuat dengan umur" << umur<<"\n" << endl;
     }
 };
+
+class pekerja : public orang {
+    public:
+    string nama;
+    pekerja(int pUmur, string pNama) :
+        orang(pUmur), nama(pNama) 
+    {
+        cout << "pekerja dibuat dengan nama " << nama << " dan umur " << umur << "\n" << endl;
+    }
+};

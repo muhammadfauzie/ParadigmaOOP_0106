04f85348e5b635ba3dfcac4fa3312ea416a0df56
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-05 13:25:43 +0700

    membuat program baseclass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -7,17 +7,3 @@ class baseClass {
         cout << "Hallo saya Function dari base Class" ;
     }
 };
-
-class derivedClass : public baseClass {
-    public:
-    void perkenalan1() override { // Override function
-        cout << "Hallo saya Function dari derived Class" ;
-    }
-};
-
-int main(){
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}

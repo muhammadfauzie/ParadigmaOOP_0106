0781f7601f5d6a29d48fd53c44cb4279ff66783b
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-05 13:22:36 +0700

    membuat program class pekerja

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -11,3 +11,13 @@ class orang {
     }
 };
 
+class pekerja : public orang {
+    public:
+    string nama;
+    pekerja(int pUmur, string pNama) :
+        orang(pUmur), nama(pNama) 
+    {
+        cout << "pekerja dibuat dengan nama " << nama << " dan umur " << umur << "\n" << endl;
+    }
+};
+
